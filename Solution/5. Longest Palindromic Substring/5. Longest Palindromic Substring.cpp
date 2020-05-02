c4ebf57c311670a220ff8ef6ce9217cd632b6bb6
jhinkoo331 <isjhinkoo@gmail.com>
2020-05-02 17:26:56 +0800

    AC. DP. Given a string s, find the longest palindromic substring in s.

---
+++ Solution/5. Longest Palindromic Substring/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+#define yes 1
+#define no 0
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+		memset(table, 0, sizeof(table));
+		max_len = 1;
+		j = 0;
+		jj = 0;
+		int len = s.size();
+		for(int offset = 0; offset < len; ++offset){
+			for(int i = 0; i < len - offset; ++i)
+				check(i, i + offset, s);
+		}
+		return s.substr(j, jj - j + 1);		
+    }
+private:
+	bool table[1000][1000];
+	int max_len = 1;
+	int j, jj;			//* first and last char of the indices of substr
+	void check(int i, int ii, const string& s){		//! i <= ii
+		if(s[i] == s[ii]){
+			if(i == ii){
+				table[i][ii] = yes;
+			}else if(i + 1 == ii){
+				table[i][ii] = yes;
+				if(ii - i + 1 > max_len){
+					max_len = ii - i + 1;
+					j = i;
+					jj = ii;
+				}
+			}else if(table[i + 1][ii - 1] == no){
+				table[i][ii] = no;
+			}else{
+				table[i][ii] = yes;
+				if(ii - i + 1 > max_len){
+					max_len = ii - i + 1;
+					j = i;
+					jj = ii;
+				}
+			}
+		}else{
+			table[i][ii] = false;
+		}
+	}
+};
+
+int main(){
+	string s = "abcda", ss = "cbbd";
+	Solution sln;
+	cout << sln.longestPalindrome(s) << endl;
+	cout << sln.longestPalindrome(ss) << endl;
+	return 0;
+}
