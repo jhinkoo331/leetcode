498ba9d051c8ef6348fce668a37c52a0da5820b2
jhinkoo331 <isjhinkoo@gmail.com>
2020-05-19 11:38:41 +0800

    66% 100% todo: better mem perf

---
+++ Solution/1220____Count_Vowels_Permutation/1220_Count_Vowels_Permutation.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+using namespace std;
+
+enum vowel {a, e, i, o, u, x};
+
+class Solution {
+public:
+	int countVowelPermutation(int n) {
+		if(n <= 0){
+			return 0;
+		}
+		vector<vector<unsigned>> v(5);
+		for(int ii = 0; ii < 5; ++ii){
+			v[ii].resize(n);
+			v[ii][0] = 1;
+		}
+		for(int ii = 1; ii < n; ++ii){
+			int iii = ii - 1;
+			v[a][ii] = v[e][iii];
+			v[e][ii] = (v[a][iii] + v[i][iii]) % quotient;
+			v[i][ii] = (v[a][iii] + v[e][iii] + v[o][iii] + v[u][iii]) % quotient;
+			v[o][ii] = (v[i][iii] + v[u][iii]) % quotient;
+			v[u][ii] = v[a][iii];
+		}
+		int sum = 0;
+		for(int i = 0; i < 5; ++i){
+			sum += v[i].back();
+			sum %= quotient;
+		}
+		return sum;
+	}
+private:
+	const int quotient = 1000000000 + 7;
+};
+
+int main(){
+	Solution sln;
+	int ret = -1;
+	ret = sln.countVowelPermutation(-1);
+	ret = sln.countVowelPermutation(0);
+	ret = sln.countVowelPermutation(1);
+	ret = sln.countVowelPermutation(5);
+	ret = sln.countVowelPermutation(144);
+	ret = sln.countVowelPermutation(0x80000000);
+	ret = sln.countVowelPermutation(0x7fffffff);
+	return 0;
+}
