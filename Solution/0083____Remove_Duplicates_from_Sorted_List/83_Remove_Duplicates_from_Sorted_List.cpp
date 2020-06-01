f07c2a9899d185d0c44e4b1ac2665343c0b71e5e
jhinkoo331 <isjhinkoo@gmail.com>
2020-06-01 16:22:11 +0800

    [83, 5]

--- Solution/0083____Remove_Duplicates_from_Sorted_List/83_Remove_Duplicates_from_Sorted_List.cpp
+++ Solution/0083____Remove_Duplicates_from_Sorted_List/83_Remove_Duplicates_from_Sorted_List.cpp
@@ -1,4 +1,5 @@
 #include "model\ListNode.h"
+#include "util\Serial.h"
 
 
 class Solution {
@@ -25,5 +26,13 @@ private:
 		return head;
 	}
 };
+
+int main(){
+	ListNode* head = nullptr;
+	Serial::de_serialize(head, "[1,2,3,3,4,4,5]");
+	Solution sln;
+	head = sln.deleteDuplicates(head);
+	return 0;
+}
 
 
