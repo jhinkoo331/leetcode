5a7be2208c64e58daceab29c03e2d6d4be0a34ae
jhinkoo331 <isjhinkoo@gmail.com>
2020-06-25 12:36:18 +0800

    [30, 5]

--- solution/0011____Container_With_Most_Water.cpp
+++ solution/0011____Container_With_Most_Water.cpp
@@ -1,4 +1,7 @@
 #include <vector>
+#include <algorithm>
+using std::max;
+using std::min;
 // #include <tuple>
 using std::vector;
 using std::pair;
@@ -7,7 +10,7 @@ using std::pair;
 class Solution {
 public:
 	int maxArea(vector<int>& height) {
-        
+		return _2(height);
 	}
 private:
 	int maxArea_1(vector<int>& height){
@@ -29,5 +32,41 @@ private:
 				
 			}
 		}
+		return 0; // make it compile
+	}
+	/**
+	 * @perf: [30, 5]
+	 * 
+	 * @param height 
+	 * @return int 
+	 */
+	int _2(vector<int>& height){
+		//* pair<height, index>
+		vector<pair<int, int>> v;
+		v.push_back({height.back(), height.size() - 1});
+		//* set up v
+		for(int i = height.size() - 2; i >= 0; --i)
+			if(height[i] > v.back().first)
+				v.push_back({height[i], i});
+		//* iterate right-ward
+		int cur_max = 0;
+		int ans = 0;
+		for(int i = 0; i < height.size(); ++i)
+			if(height[i] > cur_max){
+				cur_max = height[i];
+				for(int ii = 0; ii < v.size(); ++ii)
+					if(v[ii].second <= i)
+						break;
+					else
+						ans = max(ans, (v[ii].second - i) * min(height[i], v[ii].first));
+			}
+		return ans;
 	}
 };
+
+int main(){
+	vector<int> v = {1,8,6,2,5,4,8,3,7};
+	Solution sln;
+	sln.maxArea(v);
+	return 0;
+}
