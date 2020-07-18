5705bc915e7145eb0ecda07a7c141007e505f228
jhinkoo331 <isjhinkoo@gmail.com>
2020-07-18 18:05:45 +0800

    [4ms, 47] todo

--- solution/0092____Reverse_Linked_List_II.cpp
+++ solution/0092____Reverse_Linked_List_II.cpp
@@ -1,31 +1,61 @@
 #include "model\ListNode.h"
 #include "util\Serial.h"
+#include <stack>
 
 using namespace std;
 
 class Solution {
 public:
 	ListNode* reverseBetween(ListNode* head, int m, int n) {
-		return reverseBetween_1(head, m, n);
+		// return reverseBetween_1(head, m, n);
+		return _2(head, m, n);
 	}
 
 private:
 	ListNode* reverseBetween_1(ListNode* head, int m, int n){
+		n = n - m + 1;
+		ListNode new_head;
+		new_head.next = head;
+		ListNode* cur = &new_head;
+		for(int i = 1; i < m; ++i)
+			cur = cur->next;
+		ListNode* tail = cur;
+		ListNode* temp = cur->next;
+		while(n--){
+			tail->next = temp->next;
+			temp->next = cur->next;
+			cur->next = temp;
+			temp = tail->next;
+			tail = tail->next;
+		}
+		return new_head.next;
+	}
+	/**
+	 * @perf: [4ms, 47]
+	 * @time: O(n)
+	 * @space: O(m - n)
+	 */
+	ListNode* _2(ListNode* head, int m, int n){
+		ListNode temp;
+		temp.next = head;
+		head = &temp;
 		ListNode* pre = head;
-		ListNode* pre_next;
-		ListNode* cur = nullptr;
-		for(int i = 2; i < m; ++i){
+		for(int i = 1; i < m; ++i)
+			pre = pre->next;
+		std::stack<ListNode*> s;
+		ListNode* cur = pre->next;
+		while(m <= n){
+			s.push(cur);
+			cur = cur->next;
+			++m;
+		}
+		while(!s.empty()){
+			pre->next = s.top();
+			s.pop();
 			pre = pre->next;
 		}
-		pre_next = pre->next;
-		cur = pre_next->next;
-		for(int i = m; i < n; ++i){
-			ListNode* temp = pre_next->next;
-			pre_next->next = temp->next;
-			temp->next = pre_next;
-			pre->next = temp;
-		}
-		return head;
+		pre->next = cur;
+		return head->next;
 	}
 };
 
