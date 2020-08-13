23cc4fb9666d5a3225428bded0135808549b8939
jhinkoo331 <isjhinkoo@gmail.com>
2020-08-13 09:18:50 +0800

    [78, 95]

--- solution/0137____Single_Number_II.cpp
+++ solution/0137____Single_Number_II.cpp
@@ -11,8 +11,6 @@ private:
 	 * @brief 	(3i + 1) 1 + (3j) 0 -> 1
 	 * 		(3i + 1) 0 + (3j) 1 -> 0
 	 * @perf: [78, 95]
-	 * @param nums 
-	 * @return int 
 	 */
 	int _1(vector<int>& nums){
 		int ans = 0;
@@ -23,6 +21,24 @@ private:
 				if(e & mask)
 					++count;
 			if(count % 3 == 1)
+				ans ^= mask;
+			mask <<= 1;
+		}
+		return ans;
+	}
+	/**
+	 * @brief no perf enhancement observed compared with method 1
+	 * @perf: [78, 72]
+	 */
+	int _2(vector<int>& nums){
+		unsigned ans = 0;
+		unsigned mask = 0x1;
+		for(int i = 0; i < 32; ++i){
+			int count = 0;
+			for(int ii = 0; ii < nums.size(); ++ii)
+				if(nums[ii] & mask)
+					++count;
+			if(count % 3)
 				ans ^= mask;
 			mask <<= 1;
 		}
