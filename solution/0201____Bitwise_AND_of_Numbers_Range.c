a9c018d2bd119e4bd529eb29a7821a1c6171fad6
jhinkoo331 <isjhinkoo@gmail.com>
2020-08-15 16:14:42 +0800

    [4ms, 65]

--- solution/0201____Bitwise_AND_of_Numbers_Range.c
+++ solution/0201____Bitwise_AND_of_Numbers_Range.c
@@ -1,26 +1,40 @@
 
 /**
  * @brief bit operation
- * @perf: 83, 36
+ * @perf: 4ms, 58
  */
 int _1(int m, int n);
 
+/**
+ * @perf: 4ms, 65 
+ */
+int _2(int m, int n);
+
 int rangeBitwiseAnd(int m, int n){
-        return _1(m, n);
+        return _2(m, n);
 }
 
 
 int _1(int m, int n){
-        //* make m >= n
-        if(m < n){
-                int temp = m;
-                m = n;
-                n = temp;
-        }
         unsigned mask = 0x80000000;
         unsigned ans = 0;
         while(mask && (m & mask) == (n & mask)){
                 ans |= (mask & m);
+                mask >>= 1;
+        }
+        return ans;
+}
+
+int _2(int m, int n){
+        unsigned mask = 0x80000000;
+        unsigned ans = 0;
+        while(mask){
+                unsigned i = m & mask;
+                unsigned ii = n & mask;
+                if(i != ii)
+                        break;
+                else
+                        ans += i;
                 mask >>= 1;
         }
         return ans;
