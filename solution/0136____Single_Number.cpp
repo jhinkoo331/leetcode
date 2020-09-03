e63f76772f0f4f050581c58215abea17dfba2f32
jhinkoo331 <isjhinkoo@gmail.com>
2020-09-04 00:58:50 +0800

    [93, 80] done

--- solution/0136____Single_Number.cpp
+++ solution/0136____Single_Number.cpp
@@ -1,8 +1,26 @@
+#include "util\frequently_included.h"
+
 class Solution {
 public:
-	int singleNumber(vector<int>& nums) {
+	int singleNumber(vector<int>& nums){
+		return _2(nums);
+	}
+private:
+	/**
+	 * @brief 98, 35
+	 */
+	int _1(vector<int>& nums) {
 		for(int i = 1; i < nums.size(); ++i)
 			nums[0] ^= nums[i];
-		return nums.front();
+		return nums[0];
+	}
+	/**
+	 * @brief 93, 80
+	 */
+	int _2(vector<int>& nums){
+		int temp = 0;
+		for(auto e: nums)
+			temp ^= e;
+		return temp;
 	}
 };
