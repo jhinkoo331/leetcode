7ded83a9549337604166ed87c2b01cfd9cafc962
jhinkoo331 <isjhinkoo@gmail.com>
2020-09-05 22:35:03 +0800

    [23, 39] todo: solution

--- solution/0078____Subsets.cpp
+++ solution/0078____Subsets.cpp
@@ -2,46 +2,29 @@
 
 class Solution {
 public:
-        vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> subsets(vector<int>& nums) {
                 _1(nums);
                 return ans;
         }
 private:
         vector<vector<int>> ans;
-        vector<int> ans_temp;
-        int nums_sz;
         /**
-         * @brief dfs
-         * @perf: 85, 14
+         * @perf: 23, 39
          */
         void _1(vector<int>& nums){
                 ans.clear();
-                ans_temp.clear();
-                if(nums.empty())
-                        return;
-                sort(nums.begin(), nums.end());
-                nums_sz = nums.size();
-                nums.push_back(nums.back() ^ 1);        //* sentinel
-                _1_dfs(nums, 0);
-        }
-        void _1_dfs(vector<int>& nums, int index){
-                int next_index = index + 1;
-                while(nums[next_index] == nums[index])
-                        ++next_index;
-                //* search deeper
-                if(next_index == nums_sz)
-                        ans.push_back(ans_temp);
-                else
-                        _1_dfs(nums, next_index);
-                for(int i = 0; i < next_index - index; ++i){
-                        ans_temp.push_back(nums[index]);
-                        if(next_index == nums_sz)
-                                ans.push_back(ans_temp);
-                        else
-                                _1_dfs(nums, next_index);
+                ans.reserve(1 << nums.size());
+                ans.push_back({});
+                for(auto num: nums){
+                        int sz = ans.size();
+                        for(int i = 0; i < sz; ++i){
+                                ans.push_back(ans[i]);
+                                ans.back().push_back(num);
+                        }
                 }
-                //* restore the context
-                for(int i = 0; i < next_index - index; ++i)
-                        ans_temp.pop_back();
         }
 };
+
+int main(){
+        return 0;
+}
