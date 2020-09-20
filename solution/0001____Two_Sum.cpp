ecdf150519188302b59d61c0a3a4cbc70418aa44
jhinkoo331 <isjhinkoo@gmail.com>
2020-09-20 14:21:47 +0800

    [87, 87] done reviewed

--- solution/0001____Two_Sum.cpp
+++ solution/0001____Two_Sum.cpp
@@ -1,6 +1,4 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
+#include "util\frequently_included.h"
 
 class Solution {
 public:
@@ -10,12 +8,11 @@ public:
 private:
 	// pair<value, index>
 	vector<vector<pair<int, int>>> data;
-	vector<int> twoSum_1(vector<int>& nums, int target) {
+	vector<int> _1(vector<int>& nums, int target) {
 		data.resize(0x00010000);
 		int sz = nums.size();
-		for(int i = 0; i != sz; ++i){
-			data[nums[i] & 0x0000ffff].push_back(make_pair(nums[i], i));
-		}
+		for(int i = 0; i != sz; ++i)
+			data[nums[i] & 0x0000ffff].push_back({nums[i], i});
 		for(int i = 0; i != sz; ++i){
 			int couple = target - nums[i];				// nums[i] + couple == target
 			int couple_col = couple & 0x0000ffff;		// `couple` is stored in data[couple_col]
@@ -36,24 +33,21 @@ private:
 			int couple = target - nums[i];
 			int couple_col = couple & 0xf;
 			int sz_2 = data[couple_col].size();
-			for(int ii = 0; ii != sz_2; ++ii){
+			for(int ii = 0; ii != sz_2; ++ii)
 				if(data[couple_col][ii].first == couple && i != data[couple_col][ii].second)
 					return vector<int> {i, data[couple_col][ii].second};
-			}
 		}
 		return vector<int> {};
 	}
 	//* [87, 87]
-	vector<int> twoSum_3(vector<int>& nums, int target){
+	vector<int> _3(vector<int>& nums, int target){
 		unordered_map<int, int> mp;
 		for(int i = 0; i < nums.size(); ++i){
-			int temp = target - nums[i];
-			if(mp.count(temp)){
-				return {i, mp[temp]};
-			}else{
-				mp[nums[i]] = i;
-			}
+			auto itr = mp.find(target - nums[i]);
+			if(itr != mp.end())
+				return vector<int> {i, itr->second};
+			mp[nums[i]] = i;
 		}
-		return {};
+		return {};		//* make it compile
 	}
 };
