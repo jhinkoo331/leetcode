df1cf6334d410f2315b70da3308f7546a634c2ec
jhinkoo331 <isjhinkoo@gmail.com>
2020-10-10 10:07:49 +0800

    [95, 7.9]

---
+++ solution/0606____Construct String from Binary Tree.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	. "model"
+	"strings"
+)
+
+var sb strings.Builder
+
+// perf: 95, 7.9
+func _0606(t *TreeNode) string {
+	sb.Reset()
+	if t != nil {
+		fmt.Fprint(&sb, t.Val)
+		if t.Left != nil {
+			_0606_helper(t.Left)
+		}
+		if t.Left == nil && t.Right != nil {
+			sb.WriteString("()")
+		}
+		if t.Right != nil {
+			_0606_helper(t.Right)
+		}
+	}
+	return sb.String()
+}
+
+// t most not be nil
+func _0606_helper(t *TreeNode) {
+	fmt.Fprintf(&sb, "(%d", t.Val)
+	if t.Left != nil {
+		_0606_helper(t.Left)
+	}
+	if t.Left == nil && t.Right != nil {
+		sb.WriteString("()")
+	}
+	if t.Right != nil {
+		_0606_helper(t.Right)
+	}
+	sb.WriteRune(')')
+}
+
+func tree2str(t *TreeNode) string {
+	return _0606(t)
+}
