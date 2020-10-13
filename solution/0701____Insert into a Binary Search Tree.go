14dc1e11db42209b58be03ac2cd57c49deebaf0f
jhinkoo331 <isjhinkoo@gmail.com>
2020-10-13 19:18:19 +0800

    [75, 5]

---
+++ solution/0701____Insert into a Binary Search Tree.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	. "model"
+)
+
+// recursive
+// perf: 77, 5
+func f0701(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{val, nil, nil}
+	}
+	if root.Val < val {
+		root.Right = f0701(root.Right, val)
+	} else {
+		root.Left = f0701(root.Left, val)
+	}
+	return root
+}
+
+// iterative
+// perf: 45, 5
+func ff0701(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{val, nil, nil}
+	}
+	curRoot := root
+	for true {
+		if val < curRoot.Val {
+			if curRoot.Left == nil {
+				curRoot.Left = &TreeNode{val, nil, nil}
+				break
+			} else {
+				curRoot = curRoot.Left
+			}
+		} else {
+			if curRoot.Right == nil {
+				curRoot.Right = &TreeNode{val, nil, nil}
+				break
+			} else {
+				curRoot = curRoot.Right
+			}
+		}
+	}
+	return root
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	return f0701(root, val)
+}
+
+func main() {
+	root := &TreeNode{1, nil, nil}
+	fmt.Println(insertIntoBST(root, 2))
+}
