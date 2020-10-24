b3a2de29db15da887405acb5298a4a57e0730bf3
jhinkoo331 <isjhinkoo@gmail.com>
2020-10-24 15:15:38 +0800

    Frequently included header file added.

---
+++ util/frequently_included.h
@@ -0,0 +1,55 @@
+#ifndef frequently_included_h
+#define frequently_included_h
+
+
+
+#include <vector>
+using std::vector;
+
+#include <stack>
+using std::stack;
+
+#include <string>
+using std::string;
+
+#include <unordered_map>
+using std::unordered_map;
+#include <map>
+using std::map;
+using std::multimap;
+
+#include <set>
+using std::multiset;
+using std::set;
+
+
+#include <unordered_set>
+using std::unordered_set;
+using std::unordered_multiset;
+
+#include <cstdio>
+using std::size_t;
+
+#include <algorithm>
+using std::sort;
+using std::reverse;
+using std::min;
+using std::max;
+
+#include <queue>
+using std::queue;
+using std::priority_queue;
+
+
+
+using std::pair;
+using std::make_pair;
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+#endif
