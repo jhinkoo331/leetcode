7b283800290fcd8b8b293d837cfb4b6666014c7a
jhinkoo331 <isjhinkoo@gmail.com>
2020-10-24 13:32:07 +0800

    [95, 15]

---
+++ solution/1372____Longest ZigZag Path in a Binary Tree.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	. "model"
+)
+
+func _1372(root *TreeNode) int {
+	maxLen := 0
+	_1372_helper(root, &maxLen)
+	if maxLen == 0 {
+		return 0
+	} else {
+		return maxLen - 1
+	}
+}
+
+func _1372_helper(root *TreeNode, maxLen *int) (int, int) {
+	if root == nil {
+		return 0, 0
+	}
+	_, left := _1372_helper(root.Left, maxLen)
+	left++
+	if left > *maxLen {
+		*maxLen = left
+	}
+	right, _ := _1372_helper(root.Right, maxLen)
+	right++
+	if right > *maxLen {
+		*maxLen = right
+	}
+	return left, right
+}
+
+func longestZigZag(root *TreeNode) int {
+	return _1372(root)
+}
+
+func main() {
+
+}
