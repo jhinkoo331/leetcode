e32fa3e636f3afbd1ebecce21ee1092d261245c8
jhinkoo331 <isjhinkoo@gmail.com>
2020-10-25 19:25:24 +0800

    [4ms, 33] todo: official solution

---
+++ solution/0337____House Robber III.go
@@ -0,0 +1,40 @@
+package main
+
+import . "model"
+
+// perf: 4ms, 33
+func rob(root *TreeNode) int {
+	rob, noRob := robHelper(root)
+	if rob > noRob {
+		return rob
+	} else {
+		return noRob
+	}
+}
+
+func robHelper(root *TreeNode) (int, int) {
+	if root == nil {
+		return 0, 0
+	}
+	rob, noRob := root.Val, 0
+	temp_rob, temp_noRob := robHelper(root.Left)
+	if temp_rob > temp_noRob {
+		noRob += temp_rob
+	} else {
+		noRob += temp_noRob
+	}
+	rob += temp_noRob
+	temp_rob, temp_noRob = robHelper(root.Right)
+	if temp_rob > temp_noRob {
+		noRob += temp_rob
+	} else {
+		noRob += temp_noRob
+	}
+	rob += temp_noRob
+	return rob, noRob
+}
+
+func main() {
+	root := &TreeNode{3, &TreeNode{2, nil, &TreeNode{3, nil, nil}}, &TreeNode{3, nil, &TreeNode{1, nil, nil}}}
+	rob(root)
+}
