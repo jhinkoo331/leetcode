8f4f471992829d9836f49770a48c464f6e2fb3a5
jhinkoo331 <isjhinkoo@gmail.com>
2021-01-10 18:43:53 +0800

    [38, 27]

--- offer/68.c
+++ offer/68.c
@@ -4,13 +4,12 @@
  * @brief 38, 27, recursive.
  */
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
-        while(root){
+        while(root)
                 if(root->val > p->val && root->val > q->val)
                         root = root->left;
                 else if(root->val < p->val && root->val < q->val)
                         root = root->right;
                 else
                         break;
-        }
         return root;
 }
