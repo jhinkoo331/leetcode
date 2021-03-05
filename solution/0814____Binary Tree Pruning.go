0e7148b94d0e84d7ec9424e84c9fd2b9e658e2d5
jhinkoo331 <isjhinkoo@gmail.com>
2021-03-05 15:50:06 +0800

    [0ms, 100]

---
+++ solution/0814____Binary Tree Pruning.go
@@ -0,0 +1,32 @@
+package main
+
+import . "model"
+
+func pruneTree(root *TreeNode) *TreeNode {
+	if !e814(root) {
+		return nil
+	}
+	return root
+}
+
+// return: this tree consists at least one "1"
+// perf: 0ms, 100
+func e814(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	if !e814(root.Left) {
+		root.Left = nil
+	}
+	if !e814(root.Right) {
+		root.Right = nil
+	}
+	if root.Left == nil && root.Right == nil && root.Val == 0 {
+		return false
+	}
+	return true
+}
+
+func main() {
+
+}
