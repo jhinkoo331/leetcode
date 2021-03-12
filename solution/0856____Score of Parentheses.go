e94bf0eb6f0e34866681e8c378b10e98c4102ad9
jhinkoo331 <isjhinkoo@gmail.com>
2021-03-12 22:47:01 +0800

    [0ms, 36]

--- solution/0856____Score of Parentheses.go
+++ solution/0856____Score of Parentheses.go
@@ -1,10 +1,12 @@
 package main
 
 func scoreOfParentheses(S string) int {
-	return e856(S)
+	return ee856(S)
 }
 
 // perf: 0ms, 36
+// time: n
+// space: n
 func e856(S string) int {
 	total_score := 0
 	var scores []int
@@ -30,6 +32,25 @@ func e856(S string) int {
 		}
 	}
 	return total_score
+}
+
+// perf: 0ms, 36
+// time: n
+// space: constant
+func ee856(S string) int {
+	ans := 0
+	deepth := 0
+	for i := 0; i < len(S); i++ {
+		if S[i] == '(' {
+			if S[i+1] == ')' {
+				ans += 1 << deepth
+			}
+			deepth++
+		} else {
+			deepth--
+		}
+	}
+	return ans
 }
 
 func main() {
