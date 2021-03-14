403a2404104688eccd8388deecd6734d2bd08172
jhinkoo331 <isjhinkoo@gmail.com>
2021-03-14 11:10:00 +0800

    [10, 64] todo: iteratively

---
+++ solution/1315____Sum of Nodes with Even-Valued Grandparent.go
@@ -0,0 +1,28 @@
+package main
+
+import . "model"
+
+func sumEvenGrandparent(root *TreeNode) (ans int) {
+	if root != nil {
+		o1315(root, 1, 1, &ans)
+	}
+	return
+}
+
+// root must be non-nil
+// perf: 10, 64
+func o1315(root *TreeNode, gp, p int, ans *int) {
+	if gp%2 == 0 {
+		*ans += root.Val
+	}
+	if root.Left != nil {
+		o1315(root.Left, p, root.Val, ans)
+	}
+	if root.Right != nil {
+		o1315(root.Right, p, root.Val, ans)
+	}
+}
+
+func main() {
+
+}
