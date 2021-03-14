0d4d668604770301d90c19ea11e23340b54a6c39
jhinkoo331 <isjhinkoo@gmail.com>
2021-03-14 11:01:24 +0800

    [13, 8]

---
+++ solution/0700____Search in a Binary Search Tree.go
@@ -0,0 +1,34 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+import . "model"
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	return s700(root, val)
+}
+
+// perf: 13, 8
+func s700(root *TreeNode, val int) *TreeNode {
+	for root != nil {
+		if root.Val == val {
+			return root
+		} else if root.Val > val {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+	return nil
+}
+
+func main() {
+
+}
