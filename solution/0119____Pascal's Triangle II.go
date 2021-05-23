91daa2cec981ef827b1df85b7b66dbd624805d48
jhinkoo331 <isjhinkoo@gmail.com>
2021-05-23 13:46:20 +0800

    [0ms, 100]

--- solution/0119____Pascal's Triangle II.go
+++ solution/0119____Pascal's Triangle II.go
@@ -1,16 +1,17 @@
 package main
 
 func getRow(rowIndex int) []int {
-
+	return o119(rowIndex)
 }
 
-func _119(rowIndex int) (ans []int) {
-        ans = make([]int, 0, rowIndex + 1)
-        for {
-
-        }
-}
-
-func main() {
-	
+// perf: 0ms, 100
+// time: n
+// space: 1
+func o119(rowIndex int) (ans []int) {
+	ans = make([]int, rowIndex+1, rowIndex+1)
+	ans[0], ans[rowIndex] = 1, 1
+	for i := 1; i < rowIndex; i++ {
+		ans[i] = ans[i-1] * (rowIndex - i + 1) / i
+	}
+	return ans
 }
