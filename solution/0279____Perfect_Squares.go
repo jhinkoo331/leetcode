75164626e128a6ade4b1d5b469350c0d18595cf2
jhinkoo331 <isjhinkoo331@gmail.com>
2022-10-02 01:22:44 +0800

    [82%, 94%] todo: math

--- solution/0279____Perfect_Squares.go
+++ solution/0279____Perfect_Squares.go
@@ -1,42 +1,31 @@
 package main
 
-var t279_ps map[int]bool
-var t279_ans map[int]int
+var ans241 []int
 
+// perf: 82%, 94%
 func numSquares(n int) int {
-	return t279(n)
+	initAns241()
+	return ans241[n]
 }
 
-func t279(n int) int {
-	if t279_ps == nil {
-		t279_ps = make(map[int]bool)
-		for i := 0; i <= 100; i++ {
-			t279_ps[i*i] = true
+func initAns241() {
+	if len(ans241) > 0 {
+		return
+	}
+	ans241 = make([]int, 1e4+1, 1e4+1)
+	for i := 1; i <= 100; i += 1 {
+		ans241[i*i] = 1
+	}
+	for i := 2; i <= 1e4; i += 1 {
+		if ans241[i] == 1 {
+			continue
 		}
-	}
-	if t279_ans == nil {
-		t279_ans = make(map[int]int)
-	}
-	return t279_helper(n)
-}
-
-func t279_helper(n int) (ans int) {
-	ans = 10000000000
-	if _, ok := t279_ps[n]; ok {
-		t279_ans[n] = 1
-		return 1
-	}
-	left, right := 1, n-1
-	for ; left <= right; left, right = left+1, right-1 {
-		temp := t279_helper(left) + t279_helper(right)
-		if temp < ans {
-			ans = temp
+		curMin := i
+		for left, right := 1, i-1; left <= right; left, right = left+1, right-1 {
+			if temp := ans241[left] + ans241[right]; temp < curMin {
+				curMin = temp
+			}
 		}
+		ans241[i] = curMin
 	}
-	t279_ans[n] = ans
-	return ans
-}
-
-func main() {
-
 }
