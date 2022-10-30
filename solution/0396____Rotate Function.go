72ae93460f519c56d39d3f88c611dd8345eb61ab
jhinkoo331 <isjhinkoo331@gmail.com>
2022-10-30 19:24:03 +0800

    [95%, 63%], medium, 1 pass

--- solution/0396____Rotate Function.go
+++ solution/0396____Rotate Function.go
@@ -1,15 +1,19 @@
 package main
 
-func _0396(A []int) int {
+// perf: 95%, 63%
+func maxRotateFunction(A []int) int {
 	sum := 0 // sum of all element in the array A
 	for _, val := range A {
 		sum += val
 	}
+
 	maxVal := 0
 	preVal := 0
 	for index, val := range A { // the value of F(0)
 		preVal += index * val
 	}
+	maxVal = preVal
+
 	for _, val := range A {
 		//newAns := ans - (sum - val) + val * (len(A) - 1)
 		newVal := preVal - sum + val*len(A) // this line may raise overflow error of int32
@@ -19,10 +23,6 @@ func _0396(A []int) int {
 		preVal = newVal
 	}
 	return maxVal
-}
-
-func maxRotateFunction(A []int) int {
-	return _0396(A)
 }
 
 func main() {
