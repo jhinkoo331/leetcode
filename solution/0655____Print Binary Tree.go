acd13a8e72f52af22b24fb53c615a151faf3e2ae
jhinkoo331 <isjhinkoo331@gmail.com>
2022-10-30 19:37:05 +0800

    [4ms, 34%], medium, 2 pass

--- solution/0655____Print Binary Tree.go
+++ solution/0655____Print Binary Tree.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	. "github.com/jhinkoo331/leetcode/src/model"
+	"fmt"
 	"strconv"
 )
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 // return the height of root, if root is nil, return 0
 func treeHeight(root *TreeNode) int {
@@ -29,38 +35,37 @@ func treeHeight(root *TreeNode) int {
 }
 
 // arg t: the root node, not null
-// arg offset: the offset of this sub-tree
+// arg offset: the offset of this subtree
 // return: the width of the tree from root t
-func f0655_helper(t *TreeNode, row int, col int) {
+func f0655Helper(t *TreeNode, row int, col int) {
 	if t.Left != nil {
-		f0655_helper(t.Left, row+1, col)
+		f0655Helper(t.Left, row+1, col)
 	}
 	// shifting made by left child
 	col += 1<<(len(graph)-row-1) - 1
 	graph[row][col] = strconv.Itoa(t.Val)
 	if t.Right != nil {
-		f0655_helper(t.Right, row+1, col+1)
+		f0655Helper(t.Right, row+1, col+1)
 	}
 }
 
 var graph [][]string
 
-func f0655(root *TreeNode) [][]string {
+// perf: 4ms, 34
+func printTree(root *TreeNode) [][]string {
 	row := treeHeight(root)
 	col := 1<<row - 1
+	graph = graph[:0]
 	for i := 0; i < row; i++ {
 		graph = append(graph, make([]string, col))
 	}
 	if root != nil {
-		f0655_helper(root, 0, 0)
+		f0655Helper(root, 0, 0)
 	}
 	return graph
 }
 
-func printTree(root *TreeNode) [][]string {
-	return f0655(root)
-}
-
 func main() {
-	printTree(&TreeNode{1, &TreeNode{2, nil, &TreeNode{4, nil, nil}}, &TreeNode{3, nil, nil}})
+	temp := printTree(&TreeNode{1, &TreeNode{2, nil, &TreeNode{4, nil, nil}}, &TreeNode{3, nil, nil}})
+	fmt.Printf("%v\n", temp)
 }
